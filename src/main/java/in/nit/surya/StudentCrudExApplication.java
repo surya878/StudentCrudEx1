1dd7e8df49c9c7ddf705e0d2037c9091f223aa93
surya878 <paikaraysuryakanta@gmail.com>
2021-09-20 17:39:41 +0530

    SONALI

---
+++ src/main/java/in/nit/surya/StudentCrudExApplication.java
@@ -0,0 +1,13 @@
+package in.nit.surya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/nit/surya/StudentCrudExApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nit.surya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrudExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
